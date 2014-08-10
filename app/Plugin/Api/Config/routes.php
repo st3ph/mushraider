8edc7766fb3478f0c24d219269d72454e3233a00
Stéphane Litou <contact@stephane-litou.com>
2014-08-10 20:52:43 +0200

    Force parseExtensions to json and xml
    This avoid futur errors with patcher component

--- app/Plugin/Api/Config/routes.php
+++ app/Plugin/Api/Config/routes.php
@@ -4,7 +4,7 @@
  * REST API
  */
     Router::mapResources(array('roster', 'events', 'characters'));
-    Router::parseExtensions();
+    Router::parseExtensions('json', 'xml');
 
 /**
  * Load the CakePHP default routes. Only remove this if you do not want to use

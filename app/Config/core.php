a4cc382b36f6ee345e34cc9877f1e0ed3877d36b
Stéphane Litou <st3phh@gmail.com>
2014-12-17 17:03:11 +0100

    Remove debug mode

--- app/Config/core.php
+++ app/Config/core.php
@@ -37,8 +37,8 @@
 // TODO : generate config.salt.php
 
 
-	Configure::write('debug', 2);
-	Configure::write('mushraider', array('version' => '1.4.5', 'date' => '2014-11-25'));
+	Configure::write('debug', 0);
+	Configure::write('mushraider', array('version' => '1.4.5', 'date' => '2014-12-18'));
 
 /**
  * Configure the Error handler used to handle errors for your application. By default

8a8d818cc3877bd110dbae73e2f92a6e509a4072
Stéphane Litou <contact@stephane-litou.com>
2015-07-15 13:57:35 +0000

    Update version release date

--- app/Config/core.php
+++ app/Config/core.php
@@ -38,7 +38,7 @@
 
 
 	Configure::write('debug', 0);
-	Configure::write('mushraider', array('version' => '1.6.0', 'date' => '2015-07-18'));
+	Configure::write('mushraider', array('version' => '1.6.0', 'date' => '2015-07-15'));
 
 /**
  * Configure the Error handler used to handle errors for your application. By default
